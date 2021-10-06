f1e1fd234ec5020c3d2d4b4b482eb14f70e88a04
Reyarmenta <reynaldo.armenta18@tectijuana.edu.mx>
2021-10-05 23:06:04 -0700

    Added heroku.yml

--- api-users/entrypoint.py
+++ api-users/entrypoint.py
@@ -2,3 +2,6 @@ import os
 from app import create_app
 settings_module = os.getenv('APP_SETTINGS_MODULE')
 app = create_app(settings_module)
+
+port = int(os.environ.get("PORT", 4000))
+app.run(host='0.0.0.0', port=port)
